f477f5b920ae4a1dd520fe5b53cfab163d338e7e
Andrés Llano Duran <andres-llano@hotmail.com>
2026-01-21 19:05:41 +0100

    Implemented models interest Person & personinterestlink.

---
+++ REST API/Models/Interest.cs
@@ -0,0 +1,10 @@
+﻿namespace REST_API.Models
+{
+    public class Interest
+    {
+        public int Id { get; set; }
+        public string Title  { get; set; }
+        public string Description { get; set; }
+        public List<PersonInterestLink> Links { get; set; }
+    }
+}
---
+++ REST API/Models/PersonInterestLink.cs
@@ -0,0 +1,13 @@
+﻿namespace REST_API.Models
+{
+    public class PersonInterestLink
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public int PersonId { get; set; }
+        public int InterestId { get; set; }
+        public Interest Interest { get; set; }
+        public Person Person { get; set; }
+
+    }
+}
